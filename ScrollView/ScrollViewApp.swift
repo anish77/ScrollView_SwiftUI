106eaf804c53057f60aad67927fb80c325db0836
Ana Cvasniuc <annycvasniuc@yahoo.com>
2024-05-06 21:38:00 +0200

    Initial Commit

---
+++ ScrollView/ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewApp.swift
+//  ScrollView
+//
+//  Created by Ana Cvasniuc on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
